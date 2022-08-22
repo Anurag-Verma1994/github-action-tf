1b7132431fc4f69db0d9ef9f9b73b4ce989c422a
Anurag Verma <anurag@Anurags-MacBook-Pro.local>
2022-08-22 12:34:00 +0530

    updated provider info

--- .github/workflows/terraform/providers.tf
+++ .github/workflows/terraform/providers.tf
@@ -1,5 +1,14 @@
+terraform{
+  required_providers{
+    google = {
+      source = "hashicorp/google"
+      version = "3.84.0" # update to latest version
+    }
+  }
+}
+
 provider "google" {
   project = "${var.project}"
   region  = "${var.region}"
-  zone    = "${var.zone}"
+  zone    = "${var.zone}" # not required but recommended
 }
